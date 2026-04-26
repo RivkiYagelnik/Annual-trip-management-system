09ab5893e10760c673f73830682164b7beb5c0fd
RivkaYagelnik <rivk727@gmail.com>
2026-04-26 13:05:43 +0300

    feat(socket): add teacher rooms and build classroom cache on startup

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
+const { buildCache } = require("./classroomCache");
+
 initSocket(server);
+buildCache(); // Builds the cache once on upload
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- server/src/socket.js
+++ server/src/socket.js
@@ -10,6 +10,21 @@ function initSocket(server) {
 
   io.on("connection", (socket) => {
     console.log("Client connected:", socket.id);
+    
+    socket.on("teacher:join", async (teacherId) => {
+      socket.join(`teacher:${teacherId}`);
+      console.log(`Teacher ${teacherId} joined room`);
+
+      // Send last location immediately upon connection
+      const TeacherLocation = require("./models/TeacherLocation.model");
+      const last = await TeacherLocation.findOne({ teacherId });
+      if (last) {
+        socket.emit("teacher:location", {
+          latitude: last.latitude,
+          longitude: last.longitude,
+        });
+      }
+    });
   });
 
   return io;
